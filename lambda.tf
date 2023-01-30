1e81d5511851f60b5050843c4c593aea17e2a357
amandasds <75995105+amandasds@users.noreply.github.com>
2023-01-30 11:57:31 -0300

    retirada dos providers

--- lambda.tf
+++ lambda.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-access_key = "${var.aws_access_key}"
-secret_key = "${var.aws_secret_key}"
-region     = "${var.region}"
-}
-
 resource "aws_iam_role" "lambda_role" {
 name   = "projetoDNB"
 assume_role_policy = <<EOF
