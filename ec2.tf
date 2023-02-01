49ea925778d946624695aac98907fa3d9d05a26e
Amanda Silva <de.souza.amanda.silva@gmail.com>
2023-02-01 10:35:45 -0300

    updates

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "tls_private_key" "key" {
+/*resource "tls_private_key" "key" {
   algorithm = "RSA"
 }
 
@@ -7,18 +7,11 @@ resource "local_sensitive_file" "private_key" {
   filename          = "${var.namespace}-key.pem"
 
 }
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
 
-resource "aws_key_pair" "key_pair" {
-  key_name   = "${var.namespace}-key"
-  public_key = tls_private_key.key.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-resource "local_file" "tf-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "${var.namespace}-key"
+  key_name           = "${var.namespace}-key"
+  create_private_key = true
 }
 
 // Configure the EC2 instance in a public subnet
@@ -26,37 +19,32 @@ resource "aws_instance" "ec2_public" {
   ami                         = var.aws_ami
   associate_public_ip_address = true
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.key_pair.key_name
+  key_name                    = module.key_pair.key_pair_name
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.allow_ssh_pub.id]
 
   tags = {
-    "Name" = "${var.namespace}-EC2-PUBLIC"
+    "Name" = "${var.namespace}-EC2-PUBLI"
   }
-  //chmod key 400 on EC2 instance
-  provisioner "remote-exec" {
-    inline = ["chmod 400 ~/${aws_key_pair.key_pair.key_name}.pem"]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("${aws_key_pair.key_pair.key_name}.pem")
-      host        = self.public_ip
-    }
-  }
+  depends_on = [
+    aws_cloudtrail.checaec2
+  ]
 }
 // Configure the EC2 instance in a private subnet
 resource "aws_instance" "ec2_private" {
   ami                         = var.aws_ami
   associate_public_ip_address = false
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.key_pair.key_name
+  key_name                    = module.key_pair.key_pair_name
   subnet_id                   = module.vpc.private_subnets[1]
   vpc_security_group_ids      = [aws_security_group.allow_ssh_priv.id]
-
+  depends_on = [
+    aws_cloudtrail.checaec2
+  ]
   tags = {
     "Name" = "${var.namespace}-EC2-PRIVATE"
   }
 
 }
+*/
 
